fc1512d343699b3074178997cfe1ad2feb16591a
PaulHarden <thepharden@gmail.com>
2020-12-01 22:04:08 -0500

    initial commit

---
+++ Final Project/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public int gridXLength;
+    public int gridZLength;
+    public float gridOffset;
+    public Vector3 gridStartPoint = Vector3.zero;
+
+    void Start()
+    {
+        SpawnGridObstacle();
+    }
+
+    void SpawnGridObstacle()
+    {
+        for (int x = 0; x < gridXLength; x++)
+        {
+            for (int z = 0; z < gridZLength; z++)
+            {
+                int randomIndex = Random.Range(0, obstacles.Length);
+                Vector3 jitter = new Vector3(Random.Range(-10, 10), 0.0f, Random.Range(-10, 10));
+                Vector3 spawnPos = new Vector3(x * gridOffset + jitter.x, 0.0f, z * gridOffset + jitter.z) + gridStartPoint;
+                float randomDirection = Random.Range(0.0f, 360.0f);
+                GameObject clone = Instantiate(obstacles[randomIndex], spawnPos, Quaternion.Euler(transform.rotation.x, randomDirection, transform.rotation.z));
+            }
+        }
+    }
+}
---
+++ Final Project/Assets/Scripts/SpawnCheck.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCheck : MonoBehaviour
+{
+    //PREVENTS OBSTACLES FROM BEING SPAWNED ON TOP OF THINGS
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obstacles")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
